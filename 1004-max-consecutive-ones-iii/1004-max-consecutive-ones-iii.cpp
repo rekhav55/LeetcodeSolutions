d43486eea5044352b8768cc57729397c0d3cde8a
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-01-12 05:23:53 -0800

    Time: 82 ms (28.82%), Space: 55.4 MB (62.36%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,10 +4,13 @@ public:
         int i=0,j=0,ans=0,zero=0;
         for(i=0,j=0;j<nums.size();j++){
             if(nums[j]==0) zero++;
-            while(zero>k){
+            if(zero>k){
+                while(zero>k){
                 if(nums[i]==0) zero--;
                 i++;
+                }
             }
+            
             ans = max(ans,j-i+1);
         }
         return ans;
