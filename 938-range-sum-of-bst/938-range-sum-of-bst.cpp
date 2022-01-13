6d586335be7ac8207ba4cfdc81c65199afc5cc1f
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-01-12 20:01:47 -0800

    Time: 299 ms (5.08%), Space: 64.7 MB (16.63%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -11,11 +11,23 @@
  */
 class Solution {
 public:
+    int sum=0;
     int rangeSumBST(TreeNode* root, int low, int high) {
+        // if(root==NULL) return 0;
+        // if(root->val >= low && root->val<=high){
+        //     return root->val + rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high); 
+        // }
+        // else return rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high);
         if(root==NULL) return 0;
-        if(root->val >= low && root->val<=high){
-            return root->val + rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high); 
+        if(root->val>=low && root->val<=high){
+            sum+=root->val;
+            rangeSumBST(root->left,low,high);
+            rangeSumBST(root->right,low,high);
         }
-        else return rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high);
+        else{
+            rangeSumBST(root->left,low,high);
+            rangeSumBST(root->right,low,high);
+        }
+        return sum;
     }
 };
