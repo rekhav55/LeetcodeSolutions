b998642911b983aef775a21ce5bb9f21bf8da436
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-01-18 19:03:14 +0530

    Time: 110 ms (29.40%), Space: 23.7 MB (68.03%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -4,7 +4,7 @@ public:
         int m=matrix[0].size();
         int n=matrix.size();
         int cnt=0;
-        int dp[n][m];
+        // int dp[n][m];
         // for(int i=0;i<=n;i++)
         //     for(int j=0;j<=m;j++)
         //         dp[i][j]=0;
@@ -13,20 +13,18 @@ public:
             for(int j=0;j<m;j++)
             {
                 if(matrix[i][j]==0){
-                    dp[i][j]=0;
+                    cnt+=0;
                 }
                 else{
                     if(i==0){
-                        dp[i][j]=1;
-                        cnt+=dp[i][j];
+                        cnt+=matrix[i][j];
                     }
                     else if(j==0){
-                        dp[i][j]=1;
-                        cnt+=dp[i][j];
+                        cnt+=matrix[i][j];
                     }
                     else{
-                        dp[i][j]= min(dp[i-1][j-1], min(dp[i][j-1],dp[i-1][j]))+1;
-                        cnt+=dp[i][j];
+                        matrix[i][j]= min(matrix[i-1][j-1], min(matrix[i][j-1],matrix[i-1][j]))+1;
+                        cnt+=matrix[i][j];
                     }
                 }
             }
