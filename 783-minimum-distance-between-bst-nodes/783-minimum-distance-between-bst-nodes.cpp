760d4d89a3d83e5da611daa0dc7bf84ea37ff08b
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-01-18 10:53:54 +0530

    Time: 2 ms (64.11%), Space: 9.7 MB (59.64%) - LeetHub

--- 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.cpp
+++ 783-minimum-distance-between-bst-nodes/783-minimum-distance-between-bst-nodes.cpp
@@ -14,13 +14,10 @@ public:
     void helper(TreeNode* root, int &diff,TreeNode* &prev){
         if(root==NULL) return;
         helper(root->left,diff,prev);
-        if(prev!=nullptr){
+        if(prev){
             diff = min(diff, abs(root->val-prev->val));
-            prev=root;
         }
-        else{
-            prev=root;
-        }
+        prev=root;
         helper(root->right,diff,prev);
     }
     int minDiffInBST(TreeNode* root) {
