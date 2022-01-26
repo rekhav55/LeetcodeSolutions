7163139fac5b289646b04335c06337473ec288e9
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-01-26 21:03:58 +0530

    Time: 17 ms (9.99%), Space: 10.8 MB (14.21%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool flag=false;
+    string f;
+    void solve(string op,set<string>&st,int len){
+        if(flag) return;
+        if(op.length()==len){
+            if(st.find(op)==st.end()){
+                f=op;
+                flag=true;
+            }
+            return;
+        }
+        string op1=op;
+        string op2=op;
+        op1+="0";
+        op2+="1";
+        solve(op1,st,len);
+        solve(op2,st,len);
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        set<string>st;
+        string s = nums[0];
+        string op="";
+        int len = s.length();
+        for(auto it:nums){
+            st.insert(it);
+        }
+        solve(op,st,len);
+        return f;
+    }
+};
