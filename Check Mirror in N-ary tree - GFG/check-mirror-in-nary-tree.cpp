d3d2e936f254295c5b5af31040725f780e52e5b8
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-02-17 19:39:54 +0530

    Added solution - LeetHub

---
+++ Check Mirror in N-ary tree - GFG/check-mirror-in-nary-tree.cpp
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int checkMirrorTree(int n, int e, int A[], int B[]) {
+        // code here
+        map<int,vector<int>>mp1,mp2;
+        for(int i=0;i<2*e;i+=2){
+            mp1[A[i]].push_back(A[i+1]);
+        }
+        for(int i=0;i<2*e;i+=2){
+            mp2[B[i]].push_back(B[i+1]);
+        }
+        
+        for(auto it:mp1){
+            vector<int>temp = it.second;
+            reverse(temp.begin(),temp.end());
+            mp1[it.first]=temp;
+        }
+        
+        return mp1==mp2;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,e;
+        
+        cin>>n>>e;
+        int A[2*e], B[2*e];
+        
+        for(int i=0; i<2*e; i++)
+            cin>>A[i];
+            
+        for(int i=0; i<2*e; i++)
+            cin>>B[i];
+
+        Solution ob;
+        cout << ob.checkMirrorTree(n,e,A,B) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
