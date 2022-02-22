f90264887c8bdc10e428145b51037ef5e71626c6
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-02-22 13:33:26 +0530

    Time: 21 ms (18.46%), Space: 14.1 MB (37.89%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&image,int sr, int sc, int newColor, int oldColor){
-        if(sr<0 || sc<0 || sr>=image.size() || sc>=image[0].size() || image[sr][sc]!=oldColor || image[sr][sc]==newColor) return;
-        image[sr][sc]=newColor;
-        dfs(image,sr+1,sc,newColor,oldColor);
-        dfs(image,sr-1,sc,newColor,oldColor);
-        dfs(image,sr,sc+1,newColor,oldColor);
-        dfs(image,sr,sc-1,newColor,oldColor);
-    }
+    // void dfs(vector<vector<int>>&image,int sr, int sc, int newColor, int oldColor){
+    //     if(sr<0 || sc<0 || sr>=image.size() || sc>=image[0].size() || image[sr][sc]!=oldColor || image[sr][sc]==newColor) return;
+    //     image[sr][sc]=newColor;
+    //     dfs(image,sr+1,sc,newColor,oldColor);
+    //     dfs(image,sr-1,sc,newColor,oldColor);
+    //     dfs(image,sr,sc+1,newColor,oldColor);
+    //     dfs(image,sr,sc-1,newColor,oldColor);
+    // }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        dfs(image,sr,sc,newColor,image[sr][sc]);
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        int color = image[sr][sc];
+        image[sr][sc]=newColor;
+        int m[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+        while(!q.empty()){
+            int sz = q.size();
+            for(int i=0;i<sz;i++){
+                pair<int,int>temp = q.front();
+                q.pop();
+                int r = temp.first;
+                int c = temp.second;
+                for(int j=0;j<4;j++){
+                    int rn = r + m[j][0];
+                    int cn = c + m[j][1];
+                    if(rn>=0 && cn>=0 && rn<image.size() && cn<image[0].size() && image[rn][cn]==color && image[rn][cn]!=newColor){
+                        image[rn][cn]=newColor;
+                        q.push({rn,cn});
+                    }
+                }
+            }
+        }
         return image;
     }
 };
