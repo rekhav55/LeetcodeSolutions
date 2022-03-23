10842a12f638a2f1f6f4a6252cb30bb3a2cf8bd7
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-03-23 12:04:26 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (73.18%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+        if(target<=startValue) return startValue-target;
+        if(target%2==0) return 1 + brokenCalc(startValue, target/2);
+        return 1 + brokenCalc(startValue, target+1);
+    }
+};
