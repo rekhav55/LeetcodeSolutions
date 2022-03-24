fb4706fe98b2748b74d842a7c3b2392cf2427f92
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-03-24 08:25:09 +0530

    Time: 216 ms (5.14%), Space: 47.9 MB (5.14%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& p, int limit) {
+        sort(p.begin(),p.end());
+        int cnt=0;
+        int i=0,j=p.size()-1;
+        unordered_map<int,int>mp;
+        while(i<j){
+            if(p[i]+p[j]<=limit){
+                cnt++;
+                mp[p[i]]++;
+                mp[p[j]]++;
+                j--;
+                i++;
+            }
+            else{
+                j--;
+            }
+        }
+        for(int i=0;i<p.size();i++){
+            if(p[i]<=limit && mp.find(p[i])!=mp.end()){
+                if(mp[p[i]]==0) cnt++;
+                else mp[p[i]]--;
+            }
+            else if(p[i]<=limit && mp.find(p[i])==mp.end()) cnt++;
+        }
+        return cnt;
+    }
+};
