f7c2d2baffd6b52c363ea455ab09a7152a813fb5
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-03-24 08:28:15 +0530

    Time: 136 ms (25.78%), Space: 48 MB (5.14%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -17,13 +17,16 @@ public:
                 j--;
             }
         }
-        for(int i=0;i<p.size();i++){
-            if(p[i]<=limit && mp.find(p[i])!=mp.end()){
-                if(mp[p[i]]==0) cnt++;
-                else mp[p[i]]--;
-            }
-            else if(p[i]<=limit && mp.find(p[i])==mp.end()) cnt++;
-        }
+        // for(int i=0;i<p.size();i++){
+        //     if(p[i]<=limit && mp.find(p[i])!=mp.end()){
+        //         if(mp[p[i]]==0) cnt++;
+        //         else mp[p[i]]--;
+        //     }
+        //     else if(p[i]<=limit && mp.find(p[i])==mp.end()) cnt++;
+        // }
+        // return cnt;
+        int rem = p.size()-(cnt*2);
+        cnt = cnt+rem;
         return cnt;
     }
 };
