fc185c974463dedccd244780bb1814bf2d831d5f
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-04-08 18:34:36 +0530

    Added solution - LeetHub

--- Trapping Rain Water - GFG/trapping-rain-water.cpp
+++ Trapping Rain Water - GFG/trapping-rain-water.cpp
@@ -11,29 +11,45 @@ class Solution{
     public:
     long long trappingWater(int arr[], int n){
         // code here
-        vector<long long>left(n);
-        vector<long long>right(n);
-        left[0]=arr[0];
-        right[n-1]=arr[n-1];
-        for(int i=1;i<n;i++){
-            if(left[i-1]>arr[i]) left[i]=left[i-1];
-            else left[i]=arr[i];
-        }
-        for(int i=n-2;i>=0;i--){
-            if(right[i+1]>arr[i]) right[i]=right[i+1];
-            else right[i]=arr[i];
-        }
+        // vector<long long>left(n);
+        // vector<long long>right(n);
+        // left[0]=arr[0];
+        // right[n-1]=arr[n-1];
+        // for(int i=1;i<n;i++){
+        //     if(left[i-1]>arr[i]) left[i]=left[i-1];
+        //     else left[i]=arr[i];
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     if(right[i+1]>arr[i]) right[i]=right[i+1];
+        //     else right[i]=arr[i];
+        // }
         
-        long long sum=0;
-        vector<long long>ans(n,0);
+        // long long sum=0;
+        // vector<long long>ans(n,0);
                         
-        for(int i=0;i<n;i++){
-            if(left[i]<right[i]){
-                ans[i]= left[i]-arr[i];
+        // for(int i=0;i<n;i++){
+        //     if(left[i]<right[i]){
+        //         ans[i]= left[i]-arr[i];
+        //     }
+        //     else ans[i]=right[i]-arr[i];
+        // }
+        // for(int i=0;i<n;i++) sum+=ans[i];
+        // return sum;
+        int low = 0, high=n-1;
+        int left_max=0, right_max=0;
+        long long sum = 0;
+        while(low<=high){
+            if(arr[low]<arr[high]){
+                if(arr[low]>left_max) left_max=arr[low];
+                else sum+= left_max-arr[low];
+                low++;
             }
-            else ans[i]=right[i]-arr[i];
+            else{
+                if(arr[high]>right_max) right_max = arr[high];
+                else sum+= right_max-arr[high];
+                high--;
+            }
         }
-        for(int i=0;i<n;i++) sum+=ans[i];
         return sum;
     }
 };
