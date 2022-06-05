cc9312957ccd199aa29388b004bed37dab8c5d45
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-06-05 20:10:41 +0530

    Time: 3 ms (96.91%), Space: 7.7 MB (69.08%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int>dp(n+1);
+        dp[1]=1;
+        int p2=1;
+        int p3=1;
+        int p5=1;
+        
+        for(int i=2;i<=n;i++){
+            int f2=2*dp[p2];
+            int f3=3*dp[p3];
+            int f5=5*dp[p5];
+            
+            int mini = min({f2,f3,f5});
+            dp[i]=mini;
+            if(mini==f2){
+                p2++;
+            }
+            if(mini==f3){
+                p3++;
+            }
+            if(mini==f5){
+                p5++;
+            }
+        }
+        
+        return dp[n];
+    }
+};
