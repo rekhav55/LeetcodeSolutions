702877b1e4d98c4ac363487cc9614242fe9b3c3f
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-06-10 13:01:36 +0530

    Time: 300 ms (15.31%), Space: 73.1 MB (22.93%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -11,11 +11,26 @@ public:
         }
     }
     int longestPalindromeSubseq(string s) {
-        string temp = "";
+        string t = "";
         for(int i=s.size()-1;i>=0;i--){
-            temp+=s[i];
+            t+=s[i];
         }
         vector<vector<int>>dp(s.size()+1,vector<int>(s.size()+1,-1));
-        return lps(0,0,s,temp,dp);
+        for(int i=0;i<=s.size();i++){
+            dp[i][s.size()]=0;
+            dp[s.size()][i]=0;
+        }
+        
+        for(int i=s.size()-1;i>=0;i--){
+            for(int j=s.size()-1;j>=0;j--){
+                if(s[i]==t[j]){
+                    dp[i][j]=1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j]=max(dp[i][j+1],dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
