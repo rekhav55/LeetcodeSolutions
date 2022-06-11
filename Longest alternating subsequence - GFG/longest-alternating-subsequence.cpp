b7479bfeed008e4b059f0a71f6d46ad4fddd3ca7
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-06-11 18:42:07 +0530

    Added solution - LeetHub

---
+++ Longest alternating subsequence - GFG/longest-alternating-subsequence.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+	public:
+		int AlternatingaMaxLength(vector<int>&nums){
+		    // Code here
+		     if (nums.size() < 2)
+            return nums.size();
+        int prevdiff = nums[1] - nums[0];
+        int count = prevdiff != 0 ? 2 : 1;
+        for (int i = 2; i < nums.size(); i++) {
+            int diff = nums[i] - nums[i - 1];
+            if ((diff > 0 && prevdiff <= 0) || (diff < 0 && prevdiff >= 0)) {
+                count++;
+                prevdiff = diff;
+            }
+        }
+        return count;
+		}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution obj;
+		int ans = obj.AlternatingaMaxLength(nums);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
