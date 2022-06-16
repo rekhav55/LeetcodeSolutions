44b6383587d2b60591f6cf67c12e1ba1c7129f39
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-06-16 20:17:54 +0530

    Time: 147 ms (37.36%), Space: 12 MB (78.12%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -29,23 +29,47 @@ public:
         return cities-1;
     }
     
-    int findTheCity(int n, vector<vector<int>>& edges, int k) {
+    int findTheCity(int n, vector<vector<int>>& edges, int ki) {
         //vector<int> v;
-        vector<pair<int,int>> adj[n];
-        for(int i=0;i<edges.size();i++){
-            adj[edges[i][0]].push_back({edges[i][2],edges[i][1]});
-            adj[edges[i][1]].push_back({edges[i][2],edges[i][0]});
+//         vector<pair<int,int>> adj[n];
+//         for(int i=0;i<edges.size();i++){
+//             adj[edges[i][0]].push_back({edges[i][2],edges[i][1]});
+//             adj[edges[i][1]].push_back({edges[i][2],edges[i][0]});
 
+//         }
+//         int maxi=INT_MAX,ans;
+//         for(int i=0;i<n;i++){
+//             int res = dijkstra(adj,i,k,n);
+//             if(res<=maxi){
+//                 maxi=res;
+//                 ans = i;
+//             }
+//         }
+//         return ans;
+        vector<vector<int>>graph(n, vector(n, 100000));
+        for(int i=0; i<edges.size(); i++)
+            graph[edges[i][0]][edges[i][1]] = graph[edges[i][1]][edges[i][0]] = edges[i][2];
+        for(int i=0; i<n; i++)
+            graph[i][i] = 0;
+        
+        for(int k = 0; k < n; ++k)       // Floyd Warshalls Algo to Find All Pair Shortest Path
+         for(int i = 0; i < n; ++i)
+          for(int j = 0; j < n; ++j)  
+           graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j]);
+        
+        int ans = 0, minm = n; 
+        for(int i=0; i<n; i++)
+        {                                 // Updating the answer
+            int count = 0;
+            for(int j=0; j<n; j++)
+             if(graph[i][j]<=ki)
+               count++;
+            if(count<=minm)
+             {minm = count; ans = i;}
         }
-        int maxi=INT_MAX,ans;
-        for(int i=0;i<n;i++){
-            int res = dijkstra(adj,i,k,n);
-            if(res<=maxi){
-                maxi=res;
-                ans = i;
-            }
-        }
-        return ans;
+        
+       return ans;
+        
         
         
     }
