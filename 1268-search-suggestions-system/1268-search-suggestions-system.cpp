d9d1908707b8486cc72312db4f1858958837580f
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-06-19 07:36:54 +0530

    Time: 53 ms (89.50%), Space: 26.4 MB (85.68%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchword) {
-       unordered_map<string,vector<string>> m;
-
-    sort(products.begin(),products.end());
-    
-    for(auto x:products)
-    {
-         string s="";
-        for(int i=0;i<x.size();i++)
-        {   
-            
-            s+=x[i];
-            if(m[s].size()<3)
-            m[s].push_back(x);
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+       sort(products.begin(), products.end());
+        vector<vector<string>> ans;
+        int left = 0, right = products.size() - 1;
+        for (int i = 0; i < searchWord.length(); i++) {
+            vector<string> res;
+            char c = searchWord[i];
+            while (left <= right && (products[left].length() == i || products[left][i] < c)) left++;
+            while (left <= right && (products[right].length() == i || products[right][i] > c)) right--;
+            for (int j = 0; j < 3 && left + j <= right; j++)
+                res.push_back(products[left+j]);
+            ans.push_back(res);
         }
-    }
-    
-    vector<vector<string>> ans;
-    
-    string s="";
-    for(int i=0;i<searchword.size();i++)
-    {
-        s+=searchword[i];
-        ans.push_back(m[s]);
-    }
-    return ans;   
+        return ans;
     }
 };
