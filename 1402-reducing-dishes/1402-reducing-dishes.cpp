a88c30aa3d01f6159436f4fefd7c8fa3c0150953
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-07-01 10:13:37 +0530

    Time: 78 ms (9.19%), Space: 51.3 MB (8.77%) - LeetHub

---
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int f(int idx, int time, vector<int>&a, vector<vector<int>>&dp){
+        if(idx>=a.size()) return 0;
+        if(dp[idx][time]!=-1) return dp[idx][time];
+        int pick=0,nopick=0;
+        pick = a[idx]*time + f(idx+1,time+1,a,dp);
+        nopick = f(idx+1,time,a,dp);
+        return dp[idx][time]=max(pick,nopick);
+    }
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin(),satisfaction.end());
+        int n = satisfaction.size();
+        vector<vector<int>>dp(n+1,vector<int>(1000,-1));
+        return f(0,1,satisfaction,dp);
+    }
+};
