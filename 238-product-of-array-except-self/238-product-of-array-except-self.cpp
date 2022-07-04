a559d5817df04fbe54eec4a0d1c4ef6f367b3c62
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-07-04 14:32:25 +0530

    Time: 59 ms (7.93%), Space: 25.1 MB (15.30%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -5,33 +5,49 @@ public:
         int prod=1;
         int n = nums.size();
         vector<int>res(n);
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) zero++;
-            if(nums[i]!=0){
-                prod*=nums[i];
+        vector<int>left(n);
+        vector<int>right(n);
+        for(int i=0;i<n;i++){
+            if(i==0) left[0]=1;
+            else{
+                left[i]= left[i-1]*nums[i-1];
             }
+        }
+        right[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            right[i]=right[i+1]*nums[i+1];
         }
         
-        if(zero>1){
-            for(int i=0;i<nums.size();i++){
-                res[i]=0;
-            }
+        for(int i=0;i<n;i++){
+            res[i]=left[i]*right[i];
         }
-        if(zero==0){
-            for(int i=0;i<nums.size();i++){
-                res[i]= prod/nums[i];
-            }
-        }
-        else if(zero==1){
-            for(int i=0;i<nums.size();i++){
-                if(nums[i]==0){
-                    res[i]=prod;
-                }
-                else{
-                    res[i]=0;
-                }
-            }
-        }
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==0) zero++;
+//             if(nums[i]!=0){
+//                 prod*=nums[i];
+//             }
+//         }
+        
+//         if(zero>1){
+//             for(int i=0;i<nums.size();i++){
+//                 res[i]=0;
+//             }
+//         }
+//         if(zero==0){
+//             for(int i=0;i<nums.size();i++){
+//                 res[i]= prod/nums[i];
+//             }
+//         }
+//         else if(zero==1){
+//             for(int i=0;i<nums.size();i++){
+//                 if(nums[i]==0){
+//                     res[i]=prod;
+//                 }
+//                 else{
+//                     res[i]=0;
+//                 }
+//             }
+//         }
         return res;
     }
 };
