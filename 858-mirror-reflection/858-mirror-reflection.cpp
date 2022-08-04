f2e72cc05ab8b82ebdb7666bc12c07073e34bc0a
Rekhav Das <77041713+rekhav55@users.noreply.github.com>
2022-08-04 11:03:59 +0530

    Time: 4 ms (23.97%), Space: 6 MB (26.45%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        int g = __gcd(p,q);
+  	p/=g;
+  	q/=g;
+
+  	// logic as per the above explained pattern
+  	if(p==q) return 1;
+
+  	if(p%2==0){
+  		return 2;
+  	}else{
+  		if(q%2==0){
+  			return 0;
+  		}else{
+  			return 1;
+  		}
+  	}
+  	return 0;
+    }
+};
